a181952172b050b86027a3f8c804be9054a8ac7a
jleilani <jxleilani@gmail.com>
2020-09-29 01:37:35 -0400

    homepage refresh

---
+++ app.js
@@ -0,0 +1,10 @@
+$('.menu-toggle').on('click', function() {
+    $(this).toggleClass('open');
+    $('.nav').toggleClass('open');
+    $('.bar-start').toggleClass('open');
+    $('.bar-mid').toggleClass('open');
+    $('.bar-end').toggleClass('open');
+});
+$('.nav-link').on('click', function() {
+    $('.nav').toggleClass('open');
+})
