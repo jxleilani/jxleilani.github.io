7607ed4e40e9c62fa928b4ae225d538cd66b45a2
jleilani <jxleilani@gmail.com>
2020-11-29 17:11:35 -0500

    aos right and left

--- app.js
+++ app.js
@@ -76,22 +76,41 @@ const portfolio = [
 
 function renderPortfolio() {
   for(let i=0;i<portfolio.length;i++){
-    let item = portfolio[i];
-    const html = `
-      <div class="portfolio-item">
-        <div class="portfolio-img item-${i}" data-aos="fade-up-right">
-          <a href="${item.url}"><img src="${item.image}"></a>
-        </div>
-        <div class="portfolio-desc item-2">
-          <h6>${item.title}</h6>
-          <p>${item.desc}</p>
-          <div class="icon">
-            <a href="${item.github}"><i class="fab fa-github"></i> View Repository</a>
+    if(i % 2 == 0){
+      let item = portfolio[i];
+      const html = `
+        <div class="portfolio-item">
+          <div class="portfolio-img item-${i}" data-aos="fade-up-left">
+            <a href="${item.url}"><img src="${item.image}"></a>
+          </div>
+          <div class="portfolio-desc item-2">
+            <h6>${item.title}</h6>
+            <p>${item.desc}</p>
+            <div class="icon">
+              <a href="${item.github}"><i class="fab fa-github"></i> View Repository</a>
+            </div>
           </div>
         </div>
-      </div>
-    `;
-    $('.portfoliocontainer').append(html);
+      `;
+      $('.portfoliocontainer').append(html);
+    }else{
+      let item = portfolio[i];
+      const html = `
+        <div class="portfolio-item">
+          <div class="portfolio-img item-${i}" data-aos="fade-up-right">
+            <a href="${item.url}"><img src="${item.image}"></a>
+          </div>
+          <div class="portfolio-desc item-2">
+            <h6>${item.title}</h6>
+            <p>${item.desc}</p>
+            <div class="icon">
+              <a href="${item.github}"><i class="fab fa-github"></i> View Repository</a>
+            </div>
+          </div>
+        </div>
+      `;
+      $('.portfoliocontainer').append(html);
+    }
   }
 }
 renderPortfolio();
