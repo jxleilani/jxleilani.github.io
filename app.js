ae16b0853c169ece17e43f5795873105bf2eeaa5
jleilani <jxleilani@gmail.com>
2020-12-23 15:02:00 -0500

    add target _blank to links

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ function renderPortfolio() {
       const html = `
         <div class="portfolio-item">
           <div class="portfolio-img item-${i}" data-aos="fade-up-left">
-            <a href="${item.url}"><img src="${item.image}" alt="${item.alt}"></a>
+            <a href="${item.url}" target="_blank"><img src="${item.image}" alt="${item.alt}"></a>
           </div>
           <div class="portfolio-desc item-2">
             <h6>${item.title}</h6>
             <p>${item.desc}</p>
             <div class="icon">
-              <a href="${item.github}"><i class="fab fa-github"></i> View Repository</a>
+              <a href="${item.github}" target="_blank"><i class="fab fa-github"></i> View Repository</a>
             </div>
           </div>
         </div>
